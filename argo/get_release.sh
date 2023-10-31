73003f10db85ad3c8d134af5b00d0c4075a158e3
SaulMarcial <saulmls99@gmail.com>
2023-10-31 14:48:45 -0600

    Update datos y curl

--- argo/get_release.sh
+++ argo/get_release.sh
@@ -3,9 +3,9 @@
 # Obtiene el valor del git Release
 release=$(curl -L \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer 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" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/SaulMarcial/react-tekton-argo/releases/latest | jq -r .tag_name)
+  https://api.github.com/repos/SaulMarcial/node-web-app/releases/latest | grep tag_name | awk -F'"' '{print $4}')
 
 # Imprime el valor del Release
 echo $release
